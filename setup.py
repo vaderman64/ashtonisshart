f135357c5c37d5622bf682ef5dfb9c3aea34bad3
vaderman645 <71646638+vaderman645@users.noreply.github.com>
2021-10-01 23:40:34 -0400

    fart

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='AshtonsShitterPC',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='ajhen',
+    author_email='',
+    description=''
+)
